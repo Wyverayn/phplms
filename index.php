631a659858f5e61a632acd10bdbe8c78cf8f8e18
Wyverayn <160828067+Wyverayn@users.noreply.github.com>
2024-03-13 16:52:29 +0800

    index(may bootstrap)

--- index.php
+++ index.php
@@ -4,121 +4,112 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Libri</title>
-    <link rel = "stylesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel ="Stylesheet" href ="indexstyle.css">
     <link rel = "icon" href = "images/logoL.png">
 </head>
 
-
-<body class = "body-home">
+<body>
+<div class = "body-home">
+    <div class="bluebalda">
+      
     <div class = "black-fill">
-
         <div class ="container">
-          <br><br>
-          <nav class="navbar navbar-expand-lg bg-body-tertiary" id = "homeNav">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#welcome">
-                <img src="images/logoL.png" width = "100">
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">
-                      Home
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#about">
-                      About
-                    </a>
-                  </li>
-                </ul>
-                <ul class = "navbar-nav me-right mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Login
-                    </a>
-                  </li>
-                </ul>    
-              </div>
-            </div>
-          </nav>
+          
+        <nav class="navbar navbar-expand-lg bg-body-tertiary"
+            id = "homeNav">
+  <div class="container-fluid">
+        <a class="navbar-brand" href="#welcome">
+            <img src="images/logoL.png" width = "60">
+            
+    </a> 
 
-          <section id="welcome" class = "welcome-text" >
-            <img src = "images/logoL.png">
-            <h4>Welcome to Libri </h4>
-            <p>In PROgrammer ng Pontevedra nga si Von we trust</p>
-          </section>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+            </li>
+            </ul>
+            <ul class = "navbar-nav me-right mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="#login">Login</a>
+      </li>
+      </ul>    
+    </div>
+  </div>
+</nav>
+
+    <section id="welcome"
+        class = "welcome-text" >
+        <h4>UNLOCK KNOWLEDGE</h4>
+        <p>Explore, Learn, Succeed</p>
+        <img src="images/icon.jpg">
+    </section>
+         
+<div class="container">
+  <div class="login-container">
+    <h2>Login</h2>
+    <form action="authenticate.php" method="post">
+      <div class="mb-3">
+        <label for="username" class="form-label">Username</label>
+        <div class="input-group">
+          <span class="input-group-text"><i class="fas fa-user"></i></span>
+          <input type="text" name="uname" class="form-control" id="username" placeholder="Enter username">
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <div class="input-group">
+          <span class="input-group-text"><i class="fas fa-lock"></i></span>
+          <input type="password" name="pwd" class="form-control" id="password" placeholder="Enter password">
+        </div>
+      </div>
+      <a href="Register.php">Don't have an account? Click here</a><br><br>
+      <button type="submit" class="btn btn-primary" name="login" value="Login">Login</button>
+      
+    </form>
+  </div>
+</div>
 
 
 
-          <!-- code na diri ni ralph para sa login -->
 
-          <div class="Container">
-            <div class="Box">
-              <div>
-                <img src="logo.png" class="logo">
-              </div>
-              <div class="Title">
-                <table>
-                  <tr>
-                    <td colspan="2" align="center">
-                      <h2>LOGIN</h2>
-                    </td>
-                  </tr>
-                </table>
-              </div>
-              <div class="Form">
-                <form action="authenticate.php" method="post">
-                  <table>
-                    <tr>
-                      <td>Username</td>
-                      <td><input type="text" name="uname"></td>
-                    </tr>
-                    <tr>
-                      <td>Password</td>
-                      <td><input type="password" name="pwd"></td>
-                    </tr>
-                    <tr>
-                      <td colspan="2" align="center">
-                        <input class="submit" type="submit" name="login" value="Login">
-                        <a href="Register.php">Don't have an account? Click here</a> <!--Temporary redirect link-->
-                      </td>
-                    </tr>
-                  </table>
-                </form>
-              </div>
-            </div>
-          </div>
-
-    <!-- asta diri -->
-
-          <section id = "about" class = "d-flex justify-content-center align-items-center flex-column" >
-            <div class="card mb-3 card-1">
-			        <div class="row g-0">
-			          <div class="col-md-4">
-			            <img src="images/logoL.png" class="img-fluid rounded-start" >
-			          </div>
-			          <div class="col-md-8">
-			            <div class="card-body">
-			              <h5 class="card-title">About Us</h5>
-			              <!-- <p class="card-text"><?=$setting['about']?></p> -->
-			              <p class="card-text"><small class="text-muted">
-                      LINTI</small>
-                    </p>
-			            </div>
-			          </div>
-			        </div>
+    <section id = "about" 
+            class = "d-flex justify-content-center
+            align-items-center flex-column" >
+    <div class="card mb-3 card-1">
+			  <div class="row g-0">
+			    <div class="col-md-4">
+			      <img src="images/logoL.png"  width = "100" >
+			    </div>
+			    <div class="col-md-8">
+			      <div class="card-body">
+			        <h5 class="card-title">About Us</h5>
+			         
+			        <p class="card-text"><small class="text-muted">LIBRI is a Learning Management System (LMS) designed to facilitate, manage, and track activities and educational content in an online learning environment. Its purpose is to have a centralised platform where students and educators could have a structured environment specifically tailored for learning, therefore enhancing the learning experience and making it simpler to navigate and manage certain content. This system also allows certain users to manage their accounts, and have access to  course materials and resources provided by instructors. Overall, LIBRI is aimed towards enhancing accessibility and effectiveness of education delivery with room for improvements as time progresses.</small></p>
 			      </div>
+			    </div>
+			  </div>
+			</div>
             <div class = "copyright-text">
-              <p>Copyright &copy; 2024 LIBRI. All Rights Reserved.</p>
+                <p>Copyright &copy; 2024 LIBRI. All Rights Reserved.</p>
+              </div>
             </div>
           </section>
         </div>
-    </div>
-  <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </div> 
+</div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
