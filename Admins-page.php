42f52c874edea5bec3196c0d81844e74a084cfdb
imfinegwenchana <162296361+imfinegwenchana@users.noreply.github.com>
2024-03-13 12:41:19 +0800

    Update Admins-page.php
    ako ang PROgrammer sang Pontevedra

--- Admins-page.php
+++ Admins-page.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Admins-page.php
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,129 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>ADMIN</title>
-    <link rel="stylesheet" href="adminstyle.css">
-</head>
-<div class="header">
-    <h2>ADMIN</h2>
-</div>
-<body>
-    <div class="info">
-        <div><h4 class="text-center"><h2>Information</h2></h4></div>
-        <div class="about">
-                    <h5>Name:</h5>
-                    <h5>Level:</h5>
-                    <h5>Info:</h5>
-        </div>
-        <div class="gallery">
-                    <div class="imageItem1"></div>
-        </div>
-
-    </div>
-        
-   
-    
-         <div class="main-content"> <h3>Main Content</h3> 
-         <div class="tablefunctions">
-            <input type="text" placeholder="Search...">
-            <a href="#" class="btn btn-primary"><h4>Upload</h4></a>
-            <a href="#" class="btn btn-primary"><h4>Download</h4></a>
-            <a href="#" class="btn btn-primary"><h4>Delete</h4></a>
-         </div>
-
-            <h3>BOOKS</h3>
-                        <table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">Number</th>
-                    <th scope="col">Book Title</th>
-                    <th scope="col">Author</th>
-                    <th scope="col">Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Book1</td>
-                    <td>Clint</td>
-                    <td>May</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Book2</td>
-                    <td>Geanne</td>
-                    <td>November</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Book3</td>
-                    <td>Von</td>
-                    <td>July</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">4</th>
-                    <td>Book4</td>
-                    <td>Ralph</td>
-                    <td>September</td>
-                    </tr>
-                    <tr>
-                    <tr>
-                    <th scope="row">5</th>
-                    <td>Book5</td>
-                    <td>Vincent</td>
-                    <td>December</td>
-                    </tr>
-                    <tr>
-                </tbody>
-                </table>
-
-            </div> 
-
-            <div class="sidebar"><h3>Sidebar</h3>
-             <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-        </div>
-</body>
-
-<footer>
-    <div class="footerdes">
-        <div>
-            <thead>
-                <tr>
-                    <th><h6>contact</h6></th>
-                    <th><h6>feedback</h6></th>
-                    <th><h6>more</h6></th>
-                </tr>
-            </thead>
-        </div>
-        </div>
-</footer>
-</html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>USER</title>
     <link rel="stylesheet" href="userstyle.css">
 </head>
 <div class="header">
-    <h2>USER</h2>
+    <h2>ADMIN</h2>
 </div>
 <body>
   <div class="body">
@@ -160,7 +40,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -170,7 +50,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -180,7 +60,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -190,7 +70,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -200,7 +80,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -210,7 +90,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -220,7 +100,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -230,7 +110,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -240,7 +120,7 @@
                     Image
                   </td>
                   <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
+                    Descriptions, aaaaaupload, and download 
                     <button type="button">Download</button>
                     <button type="button">Delete</button>
                   </td>
@@ -339,4 +219,3 @@
         </div>
 </footer>
 </html>
->>>>>>> 414bb2574df7e2d32129a000c53b39e7e09dd271:admin.php
