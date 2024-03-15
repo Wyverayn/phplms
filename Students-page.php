171a9affbbf41c699fcf941a06429c14c4778380
Haruka1-c <161037180+Haruka1-C@users.noreply.github.com>
2024-03-15 16:43:30 +0800

    added div and logout, updated sidebar functions

--- Students-page.php
+++ Students-page.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Students-page.php
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,6 +26,7 @@
         
    
     
+    <div class="temp"> <!--Added div here-->
          <div class="main-content"> <h3>Main Content</h3> 
          <div class="tablefunctions">
             <input type="text" placeholder="Search...">
@@ -82,29 +82,57 @@
 
             </div> 
 
+            <!-- Scroll bar here is a must -->
             <div class="sidebar"><h3>Sidebar</h3>
              <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-        </div>
+              <div>
+                <a href="#"><h4>Mathematics</h4></a>
+              </div>
+              <div class="space"></div>
+              <div>
+                <a href="#"><h4>Physics</h4></a>
+              </div>
+              <div class="space"></div>
+              <div>
+                <a href="#"><h4>Programming</h4></a>
+              </div>
+              <div class="space"></div>
+              <div>
+                <a href="#"><h4>Electronics</h4></a>
+              </div>
+              <div class="space"></div>
+              <div>
+                <a href="#"><h4>General Education</h4></a> <!-- Here includes GEC/GEE subs -->
+              </div>
+              <div class="space"></div>
+              <div>
+                <a href="#"><h4>Research Articles</h4></a>
+              </div>
+              <div class="space"></div>
+              <div>
+                <a href="#"><h4>Entertainment</h4></a>
+              </div>
+            </div>
+    </div> <!--placed the main contents and sidebar in one container-->
+
+            <div class="body-lower">
+              <button id="logout-btn">Logout</button>
+              <script>
+                document.getElementById('logout-btn').addEventListener('click', function() {
+                  const confirmLogout = confirm('Malog out kana? Di kana magtuon haw?');
+                  if (confirmLogout) {
+                    window.location.href = 'index.php';
+                  }
+                });
+              </script>
+            </div>
+
 </body>
 
 
-<!-- Design testing,  delete if done-->
 
+
+<!-- Design testing,  delete if done-->
 <div class="des-test">
             <table class="tab-des-test">
               <tbody>
@@ -243,226 +271,3 @@
         </div>
 </footer>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>USER</title>
-    <link rel="stylesheet" href="userstyle.css">
-</head>
-<div class="header">
-    <h2>USER</h2>
-</div>
-<body>
-  <div class="body">
-         <!-- upperpart sang body ang may information -->
-    <div class="info">
-        <div><h4 class="text-center"><h2>Information</h2></h4></div>
-        <div class="about">
-                    <h5>Name:</h5>
-                    <h5>Level:</h5>
-                    <h5>Info:</h5>
-        </div>
-        <div class="gallery">
-                    <div class="imageItem1"></div>
-        </div>
-
-    </div>
-
-         <div class="main-content"> <h3>Main Content</h3> 
-         <input type="text" placeholder="Search...">
-
-    <!-- ang table -->
-            <h3>BOOKS</h3>
-            
-            <div class="des-test">
-            <table class="tab-des-test">
-              <tbody>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-                <tr class="tr-test">
-                  <td class="td-test-image">
-                    Image
-                  </td>
-                  <td class="td-test-desc">
-                    Descriptions, aaaaaupload, and download
-                    <button type="button">Download</button>
-                    <button type="button">Delete</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            
-          </div>
-            </div> 
-            <!-- end of table -->
-
-            <div class="sidebar"><h3>Sidebar</h3>
-             <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-                <div class="space"></div>
-                <div>
-                    <a href="#"><h4>Function</h4></a>
-                </div>
-        </div>
-
-    </div>
-
-</body>
-
-<footer>
-    <div class="footerdes">
-        <div>
-            <thead>
-                <tr>
-                    <th><h6>contact</h6></th>
-                    <th><h6>feedback</h6></th>
-                    <th><h6>more</h6></th>
-                </tr>
-            </thead>
-        </div>
-        </div>
-</footer>
-</html>
->>>>>>> 414bb2574df7e2d32129a000c53b39e7e09dd271:user.php
