1ab134b8a922fce59f70663d01ee54227f6305fa
RalphisCoding29 <161139061+RalphisCoding29@users.noreply.github.com>
2024-03-29 01:24:48 +0800

    Update feedback.php

--- tempfeedback/feedback.php
+++ tempfeedback/feedback.php
@@ -9,7 +9,7 @@ $email = $_POST['email'];
 $num = $_POST['num'];
 
 
-$query = mysqli_query($con, "INSERT INTO `poll`(`id`, `name`, `email`, `phone`, `feedback`, `suggestions`) VALUES ('','$name','$email','$num','$view','$comments')");
+$query = mysqli_query($conn, "INSERT INTO `poll`(`id`, `name`, `email`, `phone`, `feedback`, `suggestions`) VALUES ('','$name','$email','$num','$view','$comments')");
 echo '<script>alert("Thank You..! Your Feedback is Valuable to Us"); location.replace(document.referrer);</script>';
 
 
