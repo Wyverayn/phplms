50ababd4ccf1dd077dc6b8c061ce44a56440d3b1
imfinegwenchana <162296361+imfinegwenchana@users.noreply.github.com>
2024-04-03 14:05:55 +0800

    Update Admins-page.php

--- Admins-page.php
+++ Admins-page.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ADMIN</title>
     <link rel="stylesheet" href="adminstyle.css">
 
 </head>
